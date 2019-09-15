85f9b94ca00c1893446c1c2a73c32d3ed525cbd1
Lauren Van Allen <lvalstine@gmail.com>
2019-09-15 17:00:12 -0500

    finally

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-$ (function(){
-    $(h1).css('color', 'red');
-    $("shopping-item").click(function() {
-        event.preventDefault();
+$(function () {
+    $('ul').on('click','button.shopping-item-delete, li', function () {
+        $(this).parent().parent().detach('li');
     });
-    $ ("shopping-item-toggle").toggleClass("shopping-item__checked");
 
-});
+    $('ul').on('click','button.shopping-item-toggle, li', function () {
+        $(this).find(".shopping-item").toggleClass("shopping-item__checked");
+    });
+
+    $("#js-shopping-list-form button").click(function (e) {
+        e.preventDefault();
+        let newLi = $('li').html();
+        console.log(newLi);
+        $('li:last').after('<li>' + newLi + '</li>');
+        let userInput= $("#shopping-list-entry").val();
+        console.log(userInput);
+        $('span.shopping-item', 'li:last').text(''+ userInput + '');
+        $('input:text').val('');
+    });
+})
+// most functions/arguments don't need to have anything in parenthesis
+// some, such as 
